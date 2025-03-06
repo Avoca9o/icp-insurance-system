a15f709320568a6d73d4ac4a6e458e232e50046c
lyricsred <lyrics.red@yandex.ru>
2025-03-06 18:07:06 +0300

    add insurers list viewing

--- policy_holder_bot/bot/keyboards/back_keyboard.py
+++ policy_holder_bot/bot/keyboards/back_keyboard.py
@@ -1,12 +1,9 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
-def get_authorization_keyboard() -> InlineKeyboardMarkup:
+def get_back_keyboard() -> InlineKeyboardMarkup:
     keyboard = [
         [
             InlineKeyboardButton('🔘 Authorize', callback_data='authorize'),
-        ],
-        [
-            InlineKeyboardButton('🔘 Insurers List', callback_data='insurers_list'),
         ],
     ]
     return InlineKeyboardMarkup(keyboard)
