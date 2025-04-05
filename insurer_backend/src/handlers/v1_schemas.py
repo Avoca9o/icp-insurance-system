357e07c04ab712a21784d394826285d503928294
Avoca9o <davladimirov@edu.hse.ru>
2025-04-05 16:04:34 +0300

    add unit tests

--- insurer_backend/src/handlers/v1_schemas.py
+++ insurer_backend/src/handlers/v1_schemas.py
@@ -20,8 +20,23 @@ class GetSchemaRequest(BaseModel):
 def handle_v1_schemas(token: str = Depends(oauth2_scheme)):
     try:
         company_id = decode_jwt_token(token)
-        res = db.get_schemas(company_id)
-        return JSONResponse(content=res, status_code=200)
+        
+        # Проверяем существование компании
+        company = db.get_company(company_id)
+        if not company:
+            raise ValueError(f"Company does not exist")
+        
+        schemas = db.get_schemas(company_id)
+        # Преобразуем схемы в список словарей
+        schemas_data = [
+            {
+                "global_version_num": schema.global_version_num,
+                "company_id": schema.company_id,
+                "diagnoses_coefs": schema.diagnoses_coefs
+            }
+            for schema in schemas
+        ]
+        return JSONResponse(content=schemas_data, status_code=200)
     except ValueError as e:
         return JSONResponse(content={"message": str(e)}, status_code=400)
     except Exception as e:
